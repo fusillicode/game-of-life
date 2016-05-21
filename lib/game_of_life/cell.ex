fde2c58f2930331bfc53000b2383c0e59256b12b
fusillicode <fusillicode@gmail.com>
2016-05-21 15:53:23 +0200

    x and y...coords

--- lib/game_of_life/cell.ex
+++ lib/game_of_life/cell.ex
@@ -1,15 +1,31 @@
 defmodule GameOfLife.Cell do
   use GenServer
 
-  def new(state) do
-    GenServer.start_link(__MODULE__, state)
+  def new(state, coords) do
+    GenServer.start_link(__MODULE__, [state: state, coords: coords])
   end
 
   def alive?(pid) do
     GenServer.call(pid, :alive?)
   end
 
-  def handle_call(:alive?, _from, state) do
+  def x(pid) do
+    GenServer.call(pid, :x)
+  end
+
+  def y(pid) do
+    GenServer.call(pid, :y)
+  end
+
+  def handle_call(:alive?, _from, [state: state, coords: _]) do
     {:reply, state, state}
+  end
+
+  def handle_call(:x, _from, [state: _, coords: [x,_]]) do
+    {:reply, x, x}
+  end
+
+  def handle_call(:y, _from, [state: _, coords: [_,y]]) do
+    {:reply, y, y}
   end
 end
--- test/game_of_life/cell_test.exs
+++ test/game_of_life/cell_test.exs
@@ -2,7 +2,7 @@ defmodule GameOfLife.CellTest do
   use ExUnit.Case
 
   setup do
-    {:ok, pid} = GameOfLife.Cell.new(false)
+    {:ok, pid} = GameOfLife.Cell.new(false, [0,0])
     {:ok, pid: pid}
   end
 
@@ -15,7 +15,25 @@ defmodule GameOfLife.CellTest do
   end
 
   test "alive? returns true if the initial state is true", state do
-    {:ok, pid} = GameOfLife.Cell.new true
+    {:ok, pid} = GameOfLife.Cell.new true, [0,0]
     assert GameOfLife.Cell.alive?(pid) == true
+  end
+
+  test "x returns 0 if the initial x coord is 0", state do
+    assert GameOfLife.Cell.x(state[:pid]) == 0
+  end
+
+  test "x returns 1 if the initial x coord is 1", state do
+    {:ok, pid} = GameOfLife.Cell.new true, [1,0]
+    assert GameOfLife.Cell.x(pid) == 1
+  end
+
+  test "y returns 0 if the initial y coord is 0", state do
+    assert GameOfLife.Cell.y(state[:pid]) == 0
+  end
+
+  test "y returns 1 if the initial y coord is 1", state do
+    {:ok, pid} = GameOfLife.Cell.new true, [0,1]
+    assert GameOfLife.Cell.y(pid) == 1
   end
 end
